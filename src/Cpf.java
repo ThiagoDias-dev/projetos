7ef81b82896980e2da91f2f6e1176393784a333d
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-05-20 23:50:47 +0000

    Criei a classe main

--- src/Cpf.java
+++ src/Cpf.java
@@ -1,10 +1,21 @@
 package src;
+import java.util.Scanner;
+import java.util.*;
 
 public class Cpf {
     public static void validatorCpf() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("SEJA BEM-VINDO\n");
+        System.out.println("VAMOS CADASTRAR SEUS DADOS: \n");
+         System.out.println("***************************************\n");
+       
+        System.out.println("Me informe seu NOME: ");
+            String nome = scanner.nextLine();
 
-
+        System.out.println("Me informe seu CPF: ");
+            String cpf = scanner.nextLine();
+        
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
 package src;
+import java.util.*;
+import java.util.Scanner; 
 
 public class Main { 
+    public static void main(String[] args) {
+        System.out.println("-----SECURITY SYSTEM-----");
+        Scanner scanner = new Scanner (System.in);
+    int opcao;
+    do{
+        System.out.println("ESCOLHA UMA DAS OPÇÕES: \n");
+
+        System.out.println("1- CADASTRAR SEUS DADOS ");
+        System.out.println("2- CRIAR LOGIN/SENHA DE ACESSO ");
+        System.out.println("3- FAZER LOGIN ");
+        System.out.println("4- SAIR ");
+        
+        System.out.println("ESCOLHA UMA OPÇÃO: ");
+        
+        opcao = scanner.nextInt();
+       
+        switch (opcao) { 
+        case 1:
+            Cpf.validatorCpf();             
+                break;
+        case 2:
+            Senha.PasswordGenerator();              
+                break;
+        case 3:
+            Login.LoginGenerator();                 
+                break;
+        case 4:
+           System.out.println("SEE YOU LATER ");                 
+                break;         
+        }}
+            while (opcao != 4);
+
+            }
+        }
+    
 
     
 
-}
+
